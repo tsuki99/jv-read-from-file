27ac53770b73fc0199b2473e686f639f9e92149e
Vlad Rublenko <vladik.rar@ukr.net>
2025-09-02 23:03:16 +0300

    Added implementation to filter words starting with 'w' and sort

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIC_CHARACTER = "w";
+    private static final String SPACE = " ";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder filteredWords = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String value = bufferedReader.readLine();
+
+            while (value != null) {
+                String[] splittedWords = value.split("\\W+");
+
+                for (String currentWord : splittedWords) {
+                    if (startsWithLetterW(currentWord)) {
+                        filteredWords.append(currentWord.toLowerCase()).append(SPACE);
+                    }
+                }
+                value = bufferedReader.readLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
+
+        String[] filteredArray = filteredWords.toString().split("\\W+");
+        Arrays.sort(filteredArray);
+
+        System.out.println(Arrays.toString(filteredArray));
+        return filteredWords.isEmpty() ? new String[]{} : filteredArray;
+    }
+
+    private boolean startsWithLetterW(String word) {
+        return word.toLowerCase().startsWith(SPECIFIC_CHARACTER);
     }
 }
